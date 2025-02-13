bac662cf950acccd5a1d16934f4daa4049e24315
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-02-13 14:13:57 +0530

    added db config to and controllers to one server.js

--- server.js
+++ server.js
@@ -1,5 +1,36 @@
 import express from "express";
-import { getTodos, getTodosById, createTodo } from "./database.js";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  })
+  .promise();
+
+export const getTodos = async () => {
+  const [rows] = await pool.query("SELECT * FROM todo");
+  return rows;
+};
+
+export const getTodosById = async (id) => {
+  const [rows] = await pool.query("SELECT * FROM todo WHERE id = ?", [id]);
+  return rows[0];
+};
+
+export const createTodo = async (task, completed) => {
+  const [res] = await pool.query(
+    "INSERT INTO todo (task, completed) VALUES(?, ?)",
+    [task, completed]
+  );
+  const id = res.insertId;
+  return getTodosById(id);
+};
 
 const app = express();
 
@@ -39,4 +70,13 @@ app.post("/", async (req, res) => {
 
 app.listen(8000, () => {
   console.log("server listening at post 8000");
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error(`Unhandled Rejection: ${err.message}`);
+  console.log("Shutting down the server due to unhandled promise rejection.");
+
+  server.close(() => {
+    process.exit(1); // Exit with failure code
+  });
 });
